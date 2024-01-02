ad18485c3e5ed33ec503adf833a6017930c431f8
richardrosenthal <rros133@wgu.edu>
2024-01-01 23:18:01 -0500

    add starter chatbot code

---
+++ exercise_1/exercise_1.py
@@ -0,0 +1,14 @@
+##This is a fake chatbot based on exercise 1 of the book. 1-1-24
+print('Hello stranger, I am chatbot2024')
+print('I love Russian literature and to talk about food')
+name = input('What is your name?')
+print('Hello ' + name + '! It is so nice to meet you')
+favColor = input ('What is your favorite color?')
+print('Wow '+ favColor + '. My favorite is Tiffany Blue')
+year = input('Tell me ' + name + ', what year is it currently?')
+year = int(year)
+print('Well then ' + name + ', get ready to be under my authority for ' + year + ' more years!!')
+print('Muahahahahahahah loser')
+print('Goodbye')
+print('additional commit')
+#testing to see if visual is actually pushing to github
